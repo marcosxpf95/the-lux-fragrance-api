03cfb876d9a04e65ab8dfd2e4b2816944fa014ea
marcosxp_cit <marcosxp@ciandt.com>
2025-05-14 15:49:54 -0300

    Change the prop to nullale

--- the-lux-fragrance-api/the-lux-fragrance-api/Models/Item.cs
+++ the-lux-fragrance-api/the-lux-fragrance-api/Models/Item.cs
@@ -3,10 +3,10 @@
 public class Item
 {
     public int? Id { get; set; }
-    public string Nome { get; set; }
-    public string Descricao { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
     public decimal Preco { get; set; }
-    public string ImagemPerfume { get; set; }
-    public string ImagemReferencia { get; set; }
-    public string Categoria { get; set; }
+    public string? ImagemPerfume { get; set; }
+    public string? ImagemReferencia { get; set; }
+    public string? Categoria { get; set; }
 }
